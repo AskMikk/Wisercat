f9f681a105533d80e4045e27ab89e1483157442a
Mükail Askerov <90192553+AskMikk@users.noreply.github.com>
2024-04-06 22:07:55 +0300

    Generated components

---
+++ src/app/components/shared/ui-message/ui-message.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ui-message',
+  standalone: true,
+  imports: [],
+  templateUrl: './ui-message.component.html',
+  styleUrl: './ui-message.component.scss'
+})
+export class UiMessageComponent {
+
+}
