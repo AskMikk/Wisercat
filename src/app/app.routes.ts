f7897f6d6adbf5e229c491cf0a6e97ebaadd1775
Mükail Askerov <90192553+AskMikk@users.noreply.github.com>
2024-04-08 14:42:31 +0300

    Added routes and cleared app.component.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { UserRegistrationFormComponent } from './components/user-registration-form/user-registration-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'sessions', pathMatch: 'full' },
+    { path: 'sessions', component: UserRegistrationFormComponent }
+];
