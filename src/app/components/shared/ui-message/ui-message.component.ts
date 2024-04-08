c0e43dfdcaa0233cd0893714654ac1e9fa34af73
Mükail Askerov <90192553+AskMikk@users.noreply.github.com>
2024-04-08 15:41:52 +0300

    Created ui message component

--- src/app/components/shared/ui-message/ui-message.component.ts
+++ src/app/components/shared/ui-message/ui-message.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-ui-message',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './ui-message.component.html',
   styleUrl: './ui-message.component.scss'
 })
 export class UiMessageComponent {
-
+  @Input() text: string = '';
+  @Input() type: 'info' | 'error' | 'success' = 'info';
 }
