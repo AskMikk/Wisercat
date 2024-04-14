d5e8e41edf94a89c15a9976870a06290f72589b2
Mükail Askerov <90192553+AskMikk@users.noreply.github.com>
2024-04-14 17:48:18 +0300

    Changed styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,5 @@ import { Routes } from '@angular/router';
 import { UserRegistrationFormComponent } from './components/user-registration-form/user-registration-form.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'sessions', pathMatch: 'full' },
-    { path: 'sessions', component: UserRegistrationFormComponent }
+    { path: '', component: UserRegistrationFormComponent }
 ];
